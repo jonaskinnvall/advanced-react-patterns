2e90d38f1c1203f5b5a7d8712f6b29f9f7dc841b
Jonas Kinnvall <jonaskinnvall+github@pm.me>
2023-02-08 20:10:05 +0100

    ex5(ec2): add action types object and export for user
    - Object actionTypes let's us and the users use those parameters instead
    of strings which limits risk of typos
    - Also helps with auto-complete which even further reduces risk of error

--- src/exercise/05.js
+++ src/exercise/05.js
@@ -9,12 +9,14 @@ const callAll =
   (...args) =>
     fns.forEach(fn => fn?.(...args))
 
+const actionTypes = {toggle: 'toggle', reset: 'reset'}
+
 function toggleReducer(state, {type, initialState}) {
   switch (type) {
-    case 'toggle': {
+    case actionTypes.toggle: {
       return {on: !state.on}
     }
-    case 'reset': {
+    case actionTypes.reset: {
       return initialState
     }
     default: {
@@ -28,8 +30,8 @@ function useToggle({initialOn = false, reducer = toggleReducer} = {}) {
   const [state, dispatch] = React.useReducer(reducer, initialState)
   const {on} = state
 
-  const toggle = () => dispatch({type: 'toggle'})
-  const reset = () => dispatch({type: 'reset', initialState})
+  const toggle = () => dispatch({type: actionTypes.toggle})
+  const reset = () => dispatch({type: actionTypes.reset, initialState})
 
   function getTogglerProps({onClick, ...props} = {}) {
     return {
@@ -55,16 +57,16 @@ function useToggle({initialOn = false, reducer = toggleReducer} = {}) {
   }
 }
 
-// export {useToggle, toggleReducer}
+// export {useToggle, toggleReducer, actionTypes}
 
-// import {useToggle, toggleReducer} from './use-toggle'
+// import {useToggle, toggleReducer, actionTypes} from './use-toggle'
 
 function App() {
   const [timesClicked, setTimesClicked] = React.useState(0)
   const clickedTooMuch = timesClicked >= 4
 
   function toggleStateReducer(state, action) {
-    if (action.type === 'toggle' && clickedTooMuch) {
+    if (action.type === actionTypes.toggle && clickedTooMuch) {
       return {on: state.on}
     }
     return toggleReducer(state, action)
