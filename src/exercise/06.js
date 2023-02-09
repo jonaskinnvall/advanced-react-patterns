a62abd76d0eb8c188bd9d915c9ee76ca06a5bc92
Jonas Kinnvall <jonaskinnvall+github@pm.me>
2023-02-09 09:03:34 +0100

    ex6(ec1): add warning if user pass 'on' without 'onChange'
    - Use warning library (that React uses) to warn user to not pass 'on'
      prop without 'onChange' or 'readOnly' prop
      - We also accept 'readOnly' prop to let user set toggle as read only
        and not get a warning

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -3,6 +3,7 @@
 
 import * as React from 'react'
 import {Switch} from '../switch'
+import warning from 'warning'
 
 const callAll =
   (...fns) =>
@@ -33,12 +34,21 @@ function useToggle({
   reducer = toggleReducer,
   onChange,
   on: controlledOn,
+  readOnly = false,
 } = {}) {
   const {current: initialState} = React.useRef({on: initialOn})
   const [state, dispatch] = React.useReducer(reducer, initialState)
 
   const onIsControlled = controlledOn != null
   const on = onIsControlled ? controlledOn : state.on
+  const hasOnChange = Boolean(onChange)
+
+  React.useEffect(() => {
+    warning(
+      !(onIsControlled && !hasOnChange && !readOnly),
+      'Failed prop type: You provided a `value` prop to a form field without an `onChange` handler. This will render a read-only field. If the field should be mutable use `defaultValue`. Otherwise, set either `onChange` or `readOnly`.',
+    )
+  }, [hasOnChange, onIsControlled, readOnly])
 
   function dispatchWithOnChange(action) {
     if (!onIsControlled) {
@@ -75,10 +85,11 @@ function useToggle({
   }
 }
 
-function Toggle({on: controlledOn, onChange, initialOn, reducer}) {
+function Toggle({on: controlledOn, onChange, readOnly, initialOn, reducer}) {
   const {on, getTogglerProps} = useToggle({
     on: controlledOn,
     onChange,
+    readOnly,
     initialOn,
     reducer,
   })
